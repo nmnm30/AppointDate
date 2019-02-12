9d18f684c455e3a47d183c5db313e738ea0471ac
Syuichi Murashima <31428090+MuraAtVwnet@users.noreply.github.com>
2019-02-12 09:24:07 +0900

    Add files via upload
    1st commit

---
+++ AppointDate.ps1
@@ -0,0 +1,48 @@
+﻿############################################################
+# 指定日を曜日付きにする
+############################################################
+function Apo([string]$Date, [string]$time){
+    $strDateTime = $Date + " " + $time
+    try{
+        $DateTime = Get-Date $strDateTime
+    }
+    catch{
+        return  "$Date $time は日付として認識できません"
+    }
+
+    $YoubiTable = @{
+        "Sunday"    = "(日)"
+        "Monday"    = "(月)"
+        "Tuesday"   = "(火)"
+        "Wednesday" = "(水)"
+        "Thursday"  = "(木)"
+        "Friday"    = "(金)"
+        "Saturday"  = "(土)"
+    }
+
+    $DayOfWeek = ($DateTime.DayOfWeek).ToString()
+    $Youbi = $YoubiTable[$DayOfWeek]
+    $Year = ($DateTime.Year).ToString()
+    $Month = ($DateTime.Month).ToString()
+    $Day =  ($DateTime.Day).ToString()
+    $Hour = ($DateTime.Hour).ToString()
+    $Minute = ($DateTime.Minute).ToString("00")
+
+    if( $time -eq [string]$null){
+        $TergetDay = $Year + "年" + $Month + "月" + $Day + "日" +$Youbi
+    }
+    else{
+        $TergetDay = $Year + "年" + $Month + "月" + $Day + "日" +$Youbi +" " + $Hour + ":" + $Minute + " ～"
+    }
+
+    # S-JIS に変更
+    $OutputEncoding = [Console]::OutputEncoding
+
+    # クリップボードにコピー
+    $TergetDay | clip
+
+    # US-ASCII に戻す
+    $OutputEncoding = New-Object System.Text.ASCIIEncoding
+
+    return $TergetDay
+}
