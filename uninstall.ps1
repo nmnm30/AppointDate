672db610b2c86ba458cdd172cab474c837ec4d76
Syuichi Murashima <mura-github@vwnet.jp>
2019-10-14 12:58:12 +0900

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -5,7 +5,9 @@ $ModuleName = "AppointDate"
 $ModulePath = Join-Path (Split-Path $PROFILE -Parent) "Modules"
 
 # モジュールを配置する Path
-$NewPath = Join-Path $ModulePath $ModuleName
+$RemovePath = Join-Path $ModulePath $ModuleName
 
 # ディレクトリ削除
-Remove-Item $NewPath
+if( Test-Path $RemovePath ){
+	Remove-Item $RemovePath -Force -Recurse
+}
