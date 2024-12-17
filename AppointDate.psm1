e32c120b36e77fd7b246f8aed9ee189133103a3b
nmnm30 <stgzurs30@gmail.com>
2024-12-17 19:03:50 +0900

    Update AppointDate.psm1

--- AppointDate.psm1
+++ AppointDate.psm1
@@ -90,10 +90,10 @@ function Apo([string]$Date, [string]$time, [string]$ToTime){
         $TergetDay = $DateTime.ToString("M月d日(ddd)")
     }
     elseif( $PointToTime -eq [string]$null ){
-        $TergetDay = $DateTime.ToString("M月d日(ddd) H時m分")
+        $TergetDay = $DateTime.ToString("M月d日(ddd) H時mm分")
     }
     else{
-        $TergetDay = $DateTime.ToString("M月d日(ddd) H時m分") + $ToDateTime.ToString("HH:mm")
+        $TergetDay = $DateTime.ToString("M月d日(ddd) H時mm分") + $ToDateTime.ToString("HH:mm")
     }
 
     # クリップボードにコピー
@@ -106,7 +106,7 @@ function Apo([string]$Date, [string]$time, [string]$ToTime){
 # 現在時刻をクリップボードにセットする
 ################################################
 function now(){
-    $NowDateTime = (Get-Date).ToString("M月d日(ddd) H時m分")
+    $NowDateTime = (Get-Date).ToString("M月d日(ddd) H時mm分")
     echo $NowDateTime
     $NowDateTime | Set-Clipboard
 }
